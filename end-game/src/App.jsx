9a4c1f5665cb6f98a636063abf2c8ee9d8343f0e
thinthunaing <thinthunaing9243@gmail.com>
2026-01-17 13:33:34 +0630

    style header, language chips and game status

--- end-game/src/App.jsx
+++ end-game/src/App.jsx
@@ -1,11 +1,34 @@
 import './App.css'
+import {languages} from './language.js'
 
 function App() {
   
-
+  const language = languages.map((lang)=>{
+    const style = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    }
+    return(
+      <span key={lang.name} style={style} className='chip'>
+        {lang.name}
+      </span>
+    )
+  })
   return (
     <>
-      <h1>End Game</h1>
+      <header>
+      <h1>Assembly: Endgame</h1>
+      <p>Guess the word in under 8 attempts to keep 
+         the programming world safe from Assembly!</p>
+      </header>
+      <section className='game-status'>
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className='language-chip'>
+        {language}
+      </section>
+     
     </>
   )
 }
