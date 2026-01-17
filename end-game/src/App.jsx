0d7e5698e35be7eb349738cd43349ea58229e827
thinthunaing <thinthunaing9243@gmail.com>
2026-01-17 18:35:17 +0630

    keyboard and new game button

--- end-game/src/App.jsx
+++ end-game/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 import {languages} from './language.js'
 
 function App() {
-  
+
+  // Language Chips
   const language = languages.map((lang)=>{
     const style = {
       backgroundColor: lang.backgroundColor,
@@ -19,15 +20,29 @@ function App() {
    const [currendyWord, setCurrentWord] = useState("accomplish");
 
    const wordArray = currendyWord.split("");
-   const word = wordArray.map( letter=>{
+   const word = wordArray.map( (letter,index)=>{
     return(
-      <span key={letter} className='letter-box'>
+      <span key={index} className='letter-box'>
         {letter.toUpperCase()}
       </span>
     )
    })
+
+  //keyboard
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  const alphabetArray = alphabet.split("");
+  const keyboard = alphabetArray.map((letter)=>{
+    return(
+      <button key={letter} className='key-button'>
+        {letter.toUpperCase()}
+      </button>
+    )
+  })
+
+
   return (
     <>
+    <div className="assembly-app"> 
       <header>
       <h1>Assembly: Endgame</h1>
       <p>Guess the word in under 8 attempts to keep 
@@ -43,7 +58,11 @@ function App() {
       <section className='word-container'>
         {word}
       </section>
-     
+      <section className='keyboard-container'>
+        {keyboard}
+      </section>
+      <button className='new-game'>New Game</button>
+    </div>
     </>
   )
 }
