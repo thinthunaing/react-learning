a70369c303dad3bb91fde1600cecd760965f3548
thinthunaing <thinthunaing9243@gmail.com>
2026-01-26 13:33:07 +0630

    refactor MenuDropdown to use children instead of items prop

--- compound-components/src/Menu/Menu.jsx
+++ compound-components/src/Menu/Menu.jsx
@@ -1,18 +1,8 @@
 import React from "react"
 
 
-export default function Menu({ children, buttonText, items }) {
-    /**
-     * Note: leave the div className="menu" here and render
-     * the children inside that div. Notice this component will become
-     * significantly simpler by doing so 💡
-     * 
-     * Also, notice our state will be broken after we make 
-     * these changes - that's okay! We'll fix it soon. In the meantime,
-     * leave the useState() call and toggle() definitions alone. Your
-     * new version won't be using them, but we'll come back to them
-     * later.
-     */
+export default function Menu({ children}) {
+  
     const [open, setOpen] = React.useState(true)
 
     function toggle() {
