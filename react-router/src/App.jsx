301e1ae6f5dd1ca6104d8d873214f17ca929ddd7
thinthunaing <thinthunaing9243@gmail.com>
2026-02-21 10:41:59 +0630

    change relative route and index route

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -19,16 +19,16 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<Van />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<Van />} />
         </Route>
         <Route path="/host" element={<HostLayout />}>
-          <Route path="/host" element={<Dashboard/>} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+          <Route index element={<Dashboard/>} />
+          <Route path="income" element={<Income />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
       </Routes>
     </BrowserRouter>
